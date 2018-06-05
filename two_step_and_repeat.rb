5be1a0e9529d5a35a7b663475ba85bce5517f301
RyleeBerger <ryleehaddon24@outlook.com>
2018-06-05 21:57:35 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,17 +1,53 @@
 def first_steps
   # Write a loop that outputs "Right foot back", "Left foot back",
   # "Right foot back", and "Stop", with sleep after each
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
 end
+
+first_steps 
 
 def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
+   puts "Right foot back"
+  sleep(0.5)
+  puts "Left foot back"
+  sleep(0.5)
+  puts "Right foot back"
+  sleep(0.5)
+  puts "Stop"
+  sleep(1)
+  puts "Right foot steps right and back"
+  sleep(0.5)
+  puts "Left foot crosses over right"
+  sleep(0.5)
+  puts "Right foot steps right"
+  puts "Turn"
+  sleep(1)
 end
+
+a_few_more_steps
 
 def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
+  steps = 0
+   if steps % 2 == 0 
+  puts "Left"
+else
+puts "Right"
+sleep(0.5)
+ # Write a loop that fulfills all criteria from Part 3 of the README.md
+end
 end
 
+how_many_steps?
+
 def break_dance
+
   # Write a solution that uses the same code as how_many_steps?, but breaks the
   # loop if steps is equal to 6
 end
